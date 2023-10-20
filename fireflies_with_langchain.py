daf286cc8e0ec40dc7bf0ed58c754d85f63c8d94
Muaz <98591478+MuazAshraf@users.noreply.github.com>
2023-10-20 15:52:43 +0500

    Update fireflies_with_langchain.py

--- fireflies_with_langchain.py
+++ fireflies_with_langchain.py
@@ -29,21 +29,19 @@ load_dotenv('.env')
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_API_ENV = os.getenv('PINECONE_API_ENV')
-oauth_client_id = os.getenv("oauth_client_id")
-oauth_client_secret = os.getenv("oauth_client_secret")
 redirect_uri = "mojosolo.com"
 # Initialize pinecone and set index
 pinecone.init(
     api_key= PINECONE_API_KEY,      
 	environment=PINECONE_API_ENV     
 )
-index_name = "mojosolo-main"
+#index_name = "mojosolo-main"
+index_name = "testing"
 
 # Initialize embeddings and AI
 embeddings = OpenAIEmbeddings()
 
-# Declare agent as a global variable
-agent = None
+
 app = Flask(__name__)
 
 API_ENDPOINT = "https://api.fireflies.ai/graphql"
@@ -169,48 +167,131 @@ def save_to_pinecone():
 @app.route('/clickup_agent')
 def clickup_agent():
     return render_template('clickup_agent.html')
-# Here's where you place the print_and_run function
+
+
+# Declare agent as a global variable
+agent = None
+@app.route('/initialize_agent', methods=['POST'])
+def initialize_agent_route():
+    global agent
+    oauth_client_id = "57NNFTN3Q19ECB4GHTQ2E3ZC6W7KO8G2"
+    oauth_client_secret = "9CBIEKCMB2NSVXEQZ0AIPZK32V5HNEOUZA4PFFHQ7BVUXNHAVZSN1SV5OD5K8TY8"
+    code = "G4AKQBCSGKED6QV3UFMAY43B4SOBGRWK"
+    print("Client ID:", oauth_client_id)
+    print("Client Secret:", oauth_client_secret)
+    print("Code:", code)
+    #access_token = ClickupAPIWrapper.get_access_token(oauth_client_id, oauth_client_secret, code)
+    access_token = "72038349_e1efa1989a7901c8abcc84bd2dbc0ad6c9ef422b2278bbfd034cca1717e92255"
+    clickup_api_wrapper = ClickupAPIWrapper(access_token=access_token)
+    toolkit = ClickupToolkit.from_clickup_api_wrapper(clickup_api_wrapper)
+    print(f'Found team_id: {clickup_api_wrapper.team_id}.\nMost request require the team id, so we store it for you in the toolkit, we assume the first team in your list is the one you want. \nNote: If you know this is the wrong ID, you can pass it at initialization.')
+    llm = ChatOpenAI(temperature=0, openai_api_key="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")
+    memory = ConversationBufferMemory(memory_key="chat_history")
+    agent = initialize_agent(
+        toolkit.get_tools(), llm=llm, memory = memory, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+    return jsonify(status='success', message='Agent Initialized successfully')
+
+
+# helper function for demo
 def print_and_run(command):
     if agent is None:
-        print("Error: Agent is not initialized.")
+        print("Agent is not initialized.")
         return
-
     print('\033[94m$ COMMAND\033[0m')
     print(command)
     print('\n\033[94m$ AGENT\033[0m')
     response = agent.run(command)
-    print(response)
     print(''.join(['-']*80))
     return response
-@app.route('/initialize_agent', methods=['POST'])
-def initialize_agent_route():
-    global agent
-    data = request.json
-    oauth_client_id = data.get('oauth_client_id')
-    oauth_client_secret = data.get('oauth_client_secret')
-    code = data.get('code')
-    access_token = ClickupAPIWrapper.get_access_token(oauth_client_id, oauth_client_secret, code)
-    clickup_api_wrapper = ClickupAPIWrapper(access_token=access_token)
-    toolkit = ClickupToolkit.from_clickup_api_wrapper(clickup_api_wrapper)
-    llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-    memory = ConversationBufferMemory(memory_key="chat_history")
-    agent = initialize_agent(toolkit.get_tools(), llm=llm, memory=memory, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-    return jsonify(status='success', message='Agent Initialized successfully')
 
-@app.route('/run_command', methods=['POST'])
-def run_command_route():
-    if not agent:
+print_and_run("Get all the teams that the user is authorized to access")
+print_and_run("Get all the spaces available to the team")
+print_and_run("Get all the folders for the team")
+
+
+@app.route('/create_folder', methods=['POST'])
+def create_folder_route():
+    data = request.json
+    folder_name = data.get('folder_name')
+    
+    if agent is None:
         return jsonify(status='error', message='Agent is not initialized.')
     
-    data = request.json
-    command = data.get('command')
-    print("command",command)
-    response = agent.run(command)
+    command = f"Create a folder called '{folder_name}'"
+    response = print_and_run(command)
+    print("Folder creation response:", response)
     return jsonify(response=response)
 
-    
+
+@app.route('/create_list', methods=['POST'])
+def create_list_route():
+    if agent is None:
+        return jsonify(status='error', message='Agent is not initialized.')
+
+    data = request.json
+    listName = data.get('listName')
+    priority = data.get('priority')
+
+    # Construct the command dynamically
+    command = f"Create a list called '{listName}' with priority {priority}"
+
+    response = print_and_run(command)
+    print("List creation response:", response)
+    return jsonify(response=response)
+@app.route('/create_task', methods=['POST'])
+def create_task_route():
+    if agent is None:
+        return jsonify(status='error', message='Agent is not initialized.')
+
+    data = request.json
+    taskName = data.get('taskName')
+    description = data.get('description')
+    assigneeName = data.get('assigneeName')
+
+    # Construct the command dynamically
+    command = f"Figure out what user ID {assigneeName} is, create a task called '{taskName}', description '{description}', then assign it to {assigneeName}"
+
+    response = print_and_run(command)
+    print("Task creation response:", response)
+    return jsonify(response=response)
+@app.route('/execute_command', methods=['POST'])
+def execute_command_route():
+    if agent is None:
+        return jsonify(status='error', message='Agent is not initialized.')
+
+    data = request.json
+    command = data.get('command')
+    response = print_and_run(command)
+    print("Command creation response:", response)
+    return jsonify(response=response)
+
+roles = {
+    "David Matenaer": ["Agency Principal", "Creative Director", "Innovation/Workflow Expert"],
+    "Kira Diner": ["Senior Account Executive", "COO", "Producer/Strategy"],
+    "Chris Benjamin": ["HR", "Junior Account Executive"],
+    "Erin Abbott":["Project Manager", "Junior Account Executive"],
+    "Eric Lindholm":["SCRUM Master"],
+    "Zain Zulifqar":["Senior Developer back-end"],
+    "Saif Sohail":["Junior Front-End Developer"],
+    "Muaz Ashraf":["Junior AI/NLP/ML Developer"],
+    "Nick Michael":["Videographer, Video Editor"],
+    "Chandrick":["Senior Motion Graphics Designer/Editor"],
+    "Henry Hoegland":["AI/NLP/ML Laravel Developer Intern/Strategy"],
+    "McKinzie Plotts":["Senior Graphic Designer Part-time]"],
+    "Ian Matenaer":["Front-End Developer Intern"],
+    "Maggie Heilmann":["Office/Account Manager"]
+}
+def get_roles_for_user(name):
+    return roles.get(name, [])
+
+@app.route('/get_roles/<name>', methods=['GET'])
+def get_roles(name):
+    user_roles = get_roles_for_user(name)
+    if user_roles:
+        return jsonify(status='success', roles=user_roles)
+    else:
+        return jsonify(status='error', message=f'No roles found for {name}')
 
 if __name__ == "__main__":
-    time_str = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
-    print_and_run(f"Create a task called 'Test Task - {time_str}' with description 'This is a Test'")
     app.run(debug=True)
