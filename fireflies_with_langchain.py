f873664e61edd78933367a970686d6ab286be161
Muaz <98591478+MuazAshraf@users.noreply.github.com>
2023-11-03 18:46:29 +0500

    Update fireflies_with_langchain.py

--- fireflies_with_langchain.py
+++ fireflies_with_langchain.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 import requests
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
@@ -21,11 +21,12 @@ from langchain.memory.buffer import ConversationBufferMemory
 #clickup imports
 from langchain.utilities.clickup import ClickupAPIWrapper
 from langchain.agents.agent_toolkits.clickup.toolkit import ClickupToolkit
-
+# After you import Flask
+from flask_cors import CORS
 
 from dotenv import load_dotenv
 # Load environment variables from .env file
-load_dotenv('.env')
+load_dotenv('.env', override=True)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_API_ENV = os.getenv('PINECONE_API_ENV')
@@ -39,14 +40,15 @@ pinecone.init(
 index_name = "testing"
 
 # Initialize embeddings and AI
-embeddings = OpenAIEmbeddings()
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
 
 app = Flask(__name__)
 
+CORS(app)
 API_ENDPOINT = "https://api.fireflies.ai/graphql"
 API_KEY = "9aa5fb68-7963-448d-a257-2539cc5863fb"
-headers = {
+fireflies_headers = {
     "Authorization": f"Bearer {API_KEY}",
     "Content-Type": "application/json"
 }
@@ -68,18 +70,18 @@ data = {
     "query": query
 }
 
-response = requests.post(API_ENDPOINT, headers=headers, json=data)
+response = requests.post(API_ENDPOINT, headers=fireflies_headers, json=data)
 
-if response.status_code == 200:
-    # Successfully received data
-    print(response.json())
-else:
-    # Handle the error
-    print(f"Failed to fetch data from Fireflies. Status Code: {response.status_code}")
+# if response.status_code == 200:
+#     # Successfully received data
+#     print(response.json())
+# else:
+#     # Handle the error
+#     print(f"Failed to fetch data from Fireflies. Status Code: {response.status_code}")
 
 
 def get_latest_transcript_from_fireflies():
-    response = requests.post(API_ENDPOINT, headers=headers, json=data)
+    response = requests.post(API_ENDPOINT, headers=fireflies_headers, json=data)
     
     if response.status_code == 200:
         # Extract the transcript data from the response 
@@ -103,7 +105,7 @@ Transcript:
 {input}
 """
 summary_prompt_template = PromptTemplate(input_variables=["input"], template=transcript_summary_template)
-summary_chain = LLMChain(llm=ChatOpenAI(temperature=.7,model="gpt-3.5-turbo-16k"), prompt=summary_prompt_template)
+summary_chain = LLMChain(llm=ChatOpenAI(temperature=.7,model="gpt-4"), prompt=summary_prompt_template)
 
 project_plan_template = """
 You are an AI assistant. Given the transcript text below, extract the project plan:
@@ -112,7 +114,7 @@ Transcript:
 {input}
 """
 project_plan_prompt_template = PromptTemplate(input_variables=["input"], template=project_plan_template)
-project_plan_chain = LLMChain(llm=ChatOpenAI(temperature=.7,model="gpt-3.5-turbo-16k"), prompt=project_plan_prompt_template)
+project_plan_chain = LLMChain(llm=ChatOpenAI(temperature=.7,model="gpt-4"), prompt=project_plan_prompt_template)
 
 # Update the PromptTemplate and LLMChain setup for Project Deadline
 project_deadline_template = """
@@ -122,7 +124,7 @@ Transcript:
 {input}
 """
 project_deadline_prompt_template = PromptTemplate(input_variables=["input"], template=project_deadline_template)
-project_deadline_chain = LLMChain(llm=ChatOpenAI(temperature=.7,model="gpt-3.5-turbo-16k"), prompt=project_deadline_prompt_template)
+project_deadline_chain = LLMChain(llm=ChatOpenAI(temperature=.7,model="gpt-4"), prompt=project_deadline_prompt_template)
 
 @app.route('/')
 def index():
@@ -134,9 +136,6 @@ def get_transcript():
     if latest_transcript:
         overall_chain = SimpleSequentialChain(chains=[summary_chain,project_plan_chain, project_deadline_chain], verbose=True)
         overall_chain.run({"input": latest_transcript['text']})
-
-
-        
         # Assume each chain result is a single string containing the extracted info
         summary_result = summary_chain.run({"input": latest_transcript['text']})
         project_plan_result = project_plan_chain.run({"input": latest_transcript['text']})
@@ -174,97 +173,76 @@ agent = None
 @app.route('/initialize_agent', methods=['POST'])
 def initialize_agent_route():
     global agent
-    oauth_client_id = "57NNFTN3Q19ECB4GHTQ2E3ZC6W7KO8G2"
-    oauth_client_secret = "9CBIEKCMB2NSVXEQZ0AIPZK32V5HNEOUZA4PFFHQ7BVUXNHAVZSN1SV5OD5K8TY8"
-    code = "G4AKQBCSGKED6QV3UFMAY43B4SOBGRWK"
-    print("Client ID:", oauth_client_id)
-    print("Client Secret:", oauth_client_secret)
-    print("Code:", code)
+    # oauth_client_id = "ZSE6HZSO5NH9H308RC0K28E69TXNFZU2"
+    # oauth_client_secret = "H77LL3CNCGXU6OD2JU3J5H0YZJQ28FM1987NPQS3WJPJTNZ2VUTN1C96WJFEKRIW"
+    # code = "OP8LX1B11BKIUJAH9YSAG89C47BDBM91"
+    # print("Client ID:", oauth_client_id)
+    # print("Client Secret:", oauth_client_secret)
+    # print("Code:", code)
     #access_token = ClickupAPIWrapper.get_access_token(oauth_client_id, oauth_client_secret, code)
-    access_token = "72038349_e1efa1989a7901c8abcc84bd2dbc0ad6c9ef422b2278bbfd034cca1717e92255"
+    access_token = "44183335_507a749059557a8f5a99973e7cb8f6c85fe38a6f627cf651b934ac86dcfa98ea"
     clickup_api_wrapper = ClickupAPIWrapper(access_token=access_token)
     toolkit = ClickupToolkit.from_clickup_api_wrapper(clickup_api_wrapper)
     print(f'Found team_id: {clickup_api_wrapper.team_id}.\nMost request require the team id, so we store it for you in the toolkit, we assume the first team in your list is the one you want. \nNote: If you know this is the wrong ID, you can pass it at initialization.')
     llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-    memory = ConversationBufferMemory(memory_key="chat_history")
     agent = initialize_agent(
-        toolkit.get_tools(), llm=llm, memory = memory, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+        toolkit.get_tools(), llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
     )
     return jsonify(status='success', message='Agent Initialized successfully')
 
-
-# helper function for demo
 def print_and_run(command):
     if agent is None:
         print("Agent is not initialized.")
         return
-    print('\033[94m$ COMMAND\033[0m')
-    print(command)
-    print('\n\033[94m$ AGENT\033[0m')
+
     response = agent.run(command)
-    print(''.join(['-']*80))
-    return response
-
-print_and_run("Get all the teams that the user is authorized to access")
-print_and_run("Get all the spaces available to the team")
-print_and_run("Get all the folders for the team")
+    return str(response)  # Convert the response to a string
 
 
-@app.route('/create_folder', methods=['POST'])
-def create_folder_route():
-    data = request.json
-    folder_name = data.get('folder_name')
-    
+def create_structure_with_agent(folderName, listName, taskName, description, assigneeName):
     if agent is None:
-        return jsonify(status='error', message='Agent is not initialized.')
-    
-    command = f"Create a folder called '{folder_name}'"
-    response = print_and_run(command)
-    print("Folder creation response:", response)
-    return jsonify(response=response)
+        return "Agent is not initialized."
 
+    # Create a folder
+    folder_response = print_and_run(f"Create a folder named '{folderName}'")
 
-@app.route('/create_list', methods=['POST'])
-def create_list_route():
-    if agent is None:
-        return jsonify(status='error', message='Agent is not initialized.')
+    # Create a list inside the folder
+    list_response = print_and_run(f"No matter what but Inside that'{folderName}' folder, create a list name '{listName}'")
+
+    # Create a task inside the list
+    task_response = print_and_run(
+    f"Inside the '{listName}' list, create a task called '{taskName}' "f"with description '{description}', and assign it to {assigneeName}"
+)  # Use assigneeName
+
+    return jsonify(folder_response=folder_response, list_response=list_response, task_response=task_response)
+# Replace 'your_secret_token' with your actual secret token
+SECRET_TOKEN = 'MojoDeadWalking'
+@app.route('/create_structure_with_agent', methods=['POST'])
+def create_structure_route():
+    # Directly access the Authorization header
+    incoming_token = request.headers.get('Authorization')
+
+    # Print the incoming token
+    print("Incoming Token:", incoming_token)
+
+    # Check if the incoming token is correct
+    if incoming_token != SECRET_TOKEN:
+        return jsonify(error="Unauthorized access", message="Invalid token"), 401
 
     data = request.json
+    folderName = data.get('folderName')
     listName = data.get('listName')
-    priority = data.get('priority')
-
-    # Construct the command dynamically
-    command = f"Create a list called '{listName}' with priority {priority}"
-
-    response = print_and_run(command)
-    print("List creation response:", response)
-    return jsonify(response=response)
-@app.route('/create_task', methods=['POST'])
-def create_task_route():
-    if agent is None:
-        return jsonify(status='error', message='Agent is not initialized.')
-
-    data = request.json
     taskName = data.get('taskName')
     description = data.get('description')
-    assigneeName = data.get('assigneeName')
+    assigneeName = data.get('assigneeName')  # Use assigneeName
 
-    # Construct the command dynamically
-    command = f"Figure out what user ID {assigneeName} is, create a task called '{taskName}', description '{description}', then assign it to {assigneeName}"
+    response = create_structure_with_agent(folderName, listName, taskName, description, assigneeName)  # Use assigneeName
 
-    response = print_and_run(command)
-    print("Task creation response:", response)
-    return jsonify(response=response)
-@app.route('/execute_command', methods=['POST'])
-def execute_command_route():
-    if agent is None:
-        return jsonify(status='error', message='Agent is not initialized.')
+    # Since response is a Flask Response object, you can return it directly
+    return response
 
-    data = request.json
-    command = data.get('command')
-    response = print_and_run(command)
-    print("Command creation response:", response)
-    return jsonify(response=response)
+
+
 
 roles = {
     "David Matenaer": ["Agency Principal", "Creative Director", "Innovation/Workflow Expert"],
@@ -294,5 +272,4 @@ def get_roles(name):
         return jsonify(status='error', message=f'No roles found for {name}')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
-
+    app.run(port = 5001, debug=True)
